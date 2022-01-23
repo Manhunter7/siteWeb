721d592df28e36de3e7cef91566b1e1bade2075f
Manhunter7 <thomas.piquet7@gmail.com>
2022-01-23 17:38:21 +0100

    Initial commit siteWeb_perso

---
+++ siteweb/modules/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function (){
+    let writings = document.getElementsByTagName('p')
+    let body = document.getElementsByTagName('body')
+    let dMode = document.querySelectorAll('darkmode')
+})
